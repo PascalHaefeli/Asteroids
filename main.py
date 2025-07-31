580a5d6cc6c8d8161296024db252e24a37c53430
PascalHaefeli <pascal.haefeli02@gmail.com>
2025-07-31 17:11:35 +0200

    added player.py and circleshape.py, continued work on main.py; can now move a triangle across the screen using WASD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -7,12 +9,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    PC = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    #GameLoop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        PC.update(dt)
+        PC.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,38 @@
+from constants import *
+from circleshape import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    #Keybindings
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:    #key a -> turn left
+            self.rotate(-dt)
+        if keys[pygame.K_d]:    #key d -> turn right
+            self.rotate(dt)
+        if keys[pygame.K_w]:    #key w -> move up
+            self.move(dt)
+        if keys[pygame.K_s]:    #key s -> move down
+            self.move(-dt)
+    
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), (self.triangle()), 2) #rgb, shape and line width
